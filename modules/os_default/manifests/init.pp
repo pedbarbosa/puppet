234d81b172d9ab25803ab279767ec7fbb9eaa0ab
Pedro Barbosa <pedbarbosa@gmail.com>
2013-12-05 11:20:20 +0000

    Added more crap

--- modules/os_default/manifests/init.pp
+++ modules/os_default/manifests/init.pp
@@ -1,2 +1,25 @@
 class os_default {
+	
+	package {
+		'bash-completion':
+			ensure => latest;
+
+		'cifs-utils':
+			ensure => latest;
+
+		'telnet':
+			ensure => latest;
+	}
+
+    file {
+		#'/etc/localtime':
+			#ensure => link,
+	 		#target => '/usr/share/zoneinfo/Australia/Brisbane';
+
+        '/etc/ssh/sshd_config':
+            ensure => file;
+
+        '/etc/vim/vimrc':
+            source => 'puppet:///os_default/vimrc';
+    }
 }
--- modules/rtorrent/manifests/init.pp
+++ modules/rtorrent/manifests/init.pp
@@ -1,6 +1,15 @@
 class rtorrent {
     package {
+		'mediainfo':
+            ensure => installed;
+
         'rtorrent':
+            ensure => installed;
+
+        'screen':
+            ensure => installed;
+
+		'unrar':
             ensure => installed;
     }
 }
