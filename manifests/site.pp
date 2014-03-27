8e46417ec325cae9883a86b4fd9121718786137a
Pedro Barbosa <pedbarbosa@gmail.com>
2014-03-27 15:17:33 +1000

    Added syslog-ng

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,4 +2,5 @@ node 'morango.home.gateway' {
     include os_default
     include postfix
     include rtorrent
+    include syslog-ng
 }
---
+++ modules/syslog-ng/manifests/init.pp
@@ -0,0 +1,25 @@
+class syslog-ng {
+    package {
+        'rsyslog':
+            ensure  => absent;
+        'syslog-ng':
+            ensure  => installed;
+    }
+
+    service {
+        'syslog-ng':
+            enable  => true,
+            ensure  => running,
+            require => Package['syslog-ng'];
+    }
+
+    file {
+        '/etc/syslog-ng/syslog-ng.conf':
+            owner   => root,
+            group   => root,
+            mode    => '0644',
+            source  => 'puppet:///modules/syslog-ng/syslog-ng.conf',
+            require => Package['syslog-ng'],
+            notify  => Service['syslog-ng'];
+    }
+}
